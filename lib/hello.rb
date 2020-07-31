6be4600d6680183c2fc8f36a24b0574def1ec37e
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-31 13:17:15 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,11 @@
+
+
+# call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 def hello_t(array)
   i = 0
     while i < array.length
@@ -5,11 +13,4 @@ def hello_t(array)
       i = i + 1
   end
   array
-end
-
-# call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
 end
